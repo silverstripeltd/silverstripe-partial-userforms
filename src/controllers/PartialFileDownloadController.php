fd8e6cff02d96282f0ba2951468aabfc2e08b90a
Scott Sutherland <ssutherland@silverstripe.com>
2024-04-30 13:33:41 +1200

    move reused code to a trait

--- src/controllers/PartialFileDownloadController.php
+++ src/controllers/PartialFileDownloadController.php
@@ -2,33 +2,20 @@
 
 namespace Firesphere\PartialUserforms\Controllers;
 
-use Exception;
-use Firesphere\PartialUserforms\Forms\PasswordForm;
 use Firesphere\PartialUserforms\Models\PartialFormSubmission;
-use SilverStripe\Control\Controller;
-use SilverStripe\Control\Director;
+use Firesphere\PartialUserforms\traits\PartialSubmissionValidationTrait;
 use SilverStripe\Control\HTTPRequest;
-use SilverStripe\Control\HTTPResponse;
-use SilverStripe\Control\HTTPResponse_Exception;
-use SilverStripe\Control\Middleware\HTTPCacheControlMiddleware;
-use SilverStripe\Core\Convert;
-use SilverStripe\Core\Environment;
-use SilverStripe\Forms\Form;
-use SilverStripe\Forms\HiddenField;
-use SilverStripe\ORM\FieldType\DBField;
-use SilverStripe\ORM\FieldType\DBHTMLText;
 use SilverStripe\UserForms\Control\UserDefinedFormController;
-use SilverStripe\UserForms\Form\UserForm;
 use SilverStripe\Versioned\Versioned;
-use SilverStripe\View\Requirements;
 
 /**
- * Class PartialUserFormController
+ * Class PartialFileDownloadController
  *
  * @package Firesphere\PartialUserforms\Controllers
  */
 class PartialFileDownloadController extends UserDefinedFormController
 {
+    use PartialSubmissionValidationTrait;
     /**
      * @var array
      */
@@ -105,66 +92,5 @@ class PartialFileDownloadController extends UserDefinedFormController
             rewind($stream);
             fpassthru($stream);
         }
-    }
-
-
-    /**
-     * A little abstraction to be more readable
-     * @param HTTPRequest $request
-     * @return PartialFormSubmission|void
-     * @throws HTTPResponse_Exception
-     * @throws \SilverStripe\ORM\ValidationException
-     */
-    public function validateToken($request)
-    {
-        // Ensure this URL doesn't get picked up by HTTP caches
-        HTTPCacheControlMiddleware::singleton()->disableCache();
-
-        $key = $request->param('Key');
-        $token = $request->param('Token');
-        if (!$key || !$token) {
-            return $this->httpError(404);
-        }
-
-        /** @var PartialFormSubmission $partial */
-        $partial = PartialFormSubmission::validateKeyToken($key, $token);
-        if ($partial === false) {
-            return $this->httpError(404);
-        }
-
-        // Reload session by checking if the last session has expired
-        // or another submission has started
-        $sessionID = $request->getSession()->get(PartialSubmissionController::SESSION_KEY);
-        if (!$sessionID || $sessionID !== $partial->ID) {
-            PartialSubmissionController::reloadSession($request->getSession(), $partial->ID);
-        }
-
-        return $partial;
-    }
-
-    /**
-     * Checks whether this form is currently being used by someone else
-     * @return bool
-     */
-    public static function isLockedOut()
-    {
-        $session = Controller::curr()->getRequest()->getSession();
-        $submissionID = $session->get(PartialSubmissionController::SESSION_KEY);
-        $partial = PartialFormSubmission::get()->byID($submissionID);
-        $phpSessionID = session_id();
-
-        // If invalid sessions or if the last session was from the same user or that the recent session has expired
-        if (
-            !$submissionID
-            || !$partial
-            || !$partial->PHPSessionID
-            || ($partial->LockedOutUntil && $partial->dbObject('LockedOutUntil')->InPast())
-            || $phpSessionID === $partial->PHPSessionID
-        ) {
-            return false;
-        }
-
-        // Lockout when there's an ongoing session
-        return $partial->LockedOutUntil && $partial->dbObject('LockedOutUntil')->InFuture();
     }
 }
--- src/controllers/PartialUserFormController.php
+++ src/controllers/PartialUserFormController.php
@@ -4,13 +4,12 @@ namespace Firesphere\PartialUserforms\Controllers;
 
 use Exception;
 use Firesphere\PartialUserforms\Models\PartialFormSubmission;
-use SilverStripe\Control\Controller;
+use Firesphere\PartialUserforms\traits\PartialSubmissionValidationTrait;
 use SilverStripe\Control\HTTPRequest;
 use Firesphere\PartialUserforms\Forms\PasswordForm;
 use SilverStripe\Control\Director;
 use SilverStripe\Control\HTTPResponse;
 use SilverStripe\Control\HTTPResponse_Exception;
-use SilverStripe\Control\Middleware\HTTPCacheControlMiddleware;
 use SilverStripe\Core\Convert;
 use SilverStripe\Forms\Form;
 use SilverStripe\Forms\HiddenField;
@@ -27,6 +26,7 @@ use SilverStripe\View\Requirements;
  */
 class PartialUserFormController extends UserDefinedFormController
 {
+    use PartialSubmissionValidationTrait;
     /**
      * @var array
      */
@@ -112,67 +112,6 @@ class PartialUserFormController extends UserDefinedFormController
             'Content'     => DBField::create_field('HTMLText', $controller->Content),
             'Form'        => $form
         ]);
-    }
-
-
-    /**
-     * A little abstraction to be more readable
-     * @param HTTPRequest $request
-     * @return PartialFormSubmission|void
-     * @throws HTTPResponse_Exception
-     * @throws \SilverStripe\ORM\ValidationException
-     */
-    public function validateToken($request)
-    {
-        // Ensure this URL doesn't get picked up by HTTP caches
-        HTTPCacheControlMiddleware::singleton()->disableCache();
-
-        $key = $request->param('Key');
-        $token = $request->param('Token');
-        if (!$key || !$token) {
-            return $this->httpError(404);
-        }
-
-        /** @var PartialFormSubmission $partial */
-        $partial = PartialFormSubmission::validateKeyToken($key, $token);
-        if ($partial === false) {
-            return $this->httpError(404);
-        }
-
-        // Reload session by checking if the last session has expired
-        // or another submission has started
-        $sessionID = $request->getSession()->get(PartialSubmissionController::SESSION_KEY);
-        if (!$sessionID || $sessionID !== $partial->ID) {
-            PartialSubmissionController::reloadSession($request->getSession(), $partial->ID);
-        }
-
-        return $partial;
-    }
-
-    /**
-     * Checks whether this form is currently being used by someone else
-     * @return bool
-     */
-    public static function isLockedOut()
-    {
-        $session = Controller::curr()->getRequest()->getSession();
-        $submissionID = $session->get(PartialSubmissionController::SESSION_KEY);
-        $partial = PartialFormSubmission::get()->byID($submissionID);
-        $phpSessionID = session_id();
-
-        // If invalid sessions or if the last session was from the same user or that the recent session has expired
-        if (
-            !$submissionID
-            || !$partial
-            || !$partial->PHPSessionID
-            || ($partial->LockedOutUntil && $partial->dbObject('LockedOutUntil')->InPast())
-            || $phpSessionID === $partial->PHPSessionID
-        ) {
-            return false;
-        }
-
-        // Lockout when there's an ongoing session
-        return $partial->LockedOutUntil && $partial->dbObject('LockedOutUntil')->InFuture();
     }
 
     /**
